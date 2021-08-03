cafee898a823a4c4a5efe25b7edc9e79c74265c4
Marcos Icardo Chicote <marcosicardochicote15@gmail.com>
2021-08-04 00:25:47 +0200

    User login

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -1,8 +1,7 @@
 const User = require('../Models/user');
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const user = require('../Models/user');
+const jwt = require('jsonwebtoken');
 
 //Services
 const emailServices = require('../Services/email');
@@ -10,7 +9,7 @@ const emailServices = require('../Services/email');
 /**
  * @param {*} req 
  * @param {*} res 
- * Registro para usuario normal, en el momento que se registra se envia el email de validación(todo).
+ * Registro para usuario normal, en el momento que se registra se envia el email de validación.
  */
 function register(req, res){
     const saltRounds = process.env.SALT_ROUNDS;
@@ -42,7 +41,41 @@ function register(req, res){
         });
     });
 };
-
+/**
+ * @param {*} req 
+ * @param {*} res 
+ * Esta funcion sirve para validar un usario y que este pase a ser un usuario verificado. A partir de ahora tiene acceso a la aplicación
+ */
+function validateUser(req, res){
+    const {token} = req.query;
+    jwt.verify(token,process.env.token_userVerification,(err, decoded)=>{
+        if(err) return res.status(400).send({message:'Invalid token. Please ask for a new token.', error:err, success: false, date: Date()});
+        User.findByIdAndUpdate(decoded.userId, {$set:{userType: 'basicUserVerified'}},{new: true},(error, userUpdated)=>{
+            if(error) return res.status(404).send({message:'No user found. Get in contact with us.', success: false, date: Date()});
+            return res.status(200).send({message:'User verified succesfully.', userUpdated, success: true, date: Date()});
+        });
+    });
+}
+function login(req, res){
+    const {email} = req.body;
+    const {password} = req.body;
+    User.findOne({email},(err, user)=>{
+        if(err || !user) return res.status(404).send({message:'This email is not associated to any account.', success:false, date:Date()});
+        if(user.userType === "basicUserVerified" ||
+            user.userType === "admin" ||
+            user.userType === "shopAdmin"){
+                bcrypt.compare(password, user.password, (error, result)=>{
+                    if(error || !result) return res.status(400).send({message: 'Email or password invalid. Please try again.', success: false, error, date: Date()});
+                    const token = jwt.sign({id: user._id, email, userType: user.userType}, process.env.token_login ,{expiresIn: '2h'});
+                    return res.status(200).send({message:'Logged in successfully.',token,expiresIn: '2h', success: true, date: Date()});
+                });
+            } else{
+                return res.status(400).send({message: 'User is not verified. Please verify your email and try again.', success: false, date: Date()});
+            }
+    });
+}
 module.exports = {
-    register
+    register,
+    validateUser,
+    login
 }
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,11 @@ const userControllers = require('../Controllers/userControllers');
 
 const router = express.Router(); 
 
+//Registro de un usuario normal
 router.post('/register', userControllers.register);
+//Validación del email de un usuario
+router.patch('/validateEmail', userControllers.validateUser);
+//Login del usuario, se le entrega un token
+router.post('/login', userControllers.login);
 
 module.exports = router;
