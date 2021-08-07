d838f0cb89bbe8c410db6cd395aeceaacfe79a40
Marcos Icardo Chicote <marcosicardochicote15@gmail.com>
2021-08-07 20:31:45 +0200

    Postal  code utilities

--- Controllers/postalCodeControllers.js
+++ Controllers/postalCodeControllers.js
@@ -10,6 +10,36 @@ function createPostalCode(req, res){
         return res.status(200).send({message:'Postal code saved successfully.',postalCodeSaved, success:true, date:Date()});
     });
 }
+async function getPostalCode(req, res){
+    try{
+        const code = req.body.postalCode;
+        const postalCodedb = await postalCode.findOne({code});
+        if(!postalCodedb) return res.status(404).send({message:'Postal code not found. Invalid postal code.', success: false, date:Date()});
+        return res.status(200).send({message:'Postal code found successfully. This postal code is Valid.', postalCode: postalCodedb, success: true, date:Date()});
+    }catch(error){
+        return res.status(500).send({message:'Internal server error.', success:false, date:Date()});
+    }
+}
+async function getAllPostalCodes(req, res){
+    try{
+        const codes = await postalCode.find({});
+        if(!codes) return res.status(404).send({message:'There are no postal codes.', success: true, date:Date()});
+        return res.status(200).send({message:'Postal codes found successfully.', postalCodes: codes, success: true, date:Date()});
+    }catch(error){
+        return res.status(500).send({message:'Internal server error.', success:false, date:Date()});
+    }
+}
+function deletePostalCode(req, res){
+    const code = req.body.postalCode;
+    postalCode.findOneAndDelete({code},(err, codeDeleted)=>{
+        if(err) return res.status(500).send({message:'Internal server error.', success:false, date:Date()});
+        if(!codeDeleted) return res.status(404).send({message:'Postal code not found.', success: false, date:Date()});
+        return res.status(200).send({message:'Postal code deleted successfully.', codeDeleted, success: true, date:Date()});
+    })
+}
 module.exports = {
-    createPostalCode
+    createPostalCode,
+    getPostalCode,
+    getAllPostalCodes,
+    deletePostalCode
 }
--- Routes/postalCodeRoutes.js
+++ Routes/postalCodeRoutes.js
@@ -4,6 +4,13 @@ const checkAuth = require('../Middlewares/checkAuth');
 
 const router = require('./userRoutes');
 
+//Para crear un codigo postañ
 router.post('/createPostalCode', checkAuth.checkLoggedIn, checkAuth.checkIfAdmin, postalCodeControllers.createPostalCode);
+//Para obtener un codigo postal
+router.get('/getPostalCode', postalCodeControllers.getPostalCode);
+//Para obtener todos los codigos postales
+router.get('/getAllPostalCodes', postalCodeControllers.getAllPostalCodes);
+//Para eliminar un codigo postal
+router.delete('/deletePostalCode', checkAuth.checkLoggedIn, checkAuth.checkIfAdmin, postalCodeControllers.deletePostalCode);
 
 module.exports = router;
